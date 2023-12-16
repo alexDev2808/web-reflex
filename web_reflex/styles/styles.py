6f48eef78c6b0e7ed0a1b4ba1067a8b315740127
alexDev2808 <colores.2808.8000@gmail.com>
2023-12-16 09:06:26 -0600

    Empezando con estilos y separando en archivos

---
+++ web_reflex/styles/styles.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+# Constant
+MAX_WIDTH = "760px"
+
+
+# Sizes
+
+class Spacer(Enum):
+    SMALL = "0.5em"
+    DEFAULT = "1em"
+    BIG = "2em"
--- web_reflex/web_reflex.py
+++ web_reflex/web_reflex.py
@@ -3,6 +3,7 @@ from web_reflex.components.navbar import navbar
 from web_reflex.views.header.header import header
 from web_reflex.views.links.links import links
 from web_reflex.components.footer import footer
+import web_reflex.styles.styles as styles
 
 
 class State(rx.State):
@@ -10,12 +11,21 @@ class State(rx.State):
 
 
 def index() -> rx.Component:
-    return rx.vstack(
-        navbar(),
-        header(),
-        links(),
-        footer()
+    return rx.box(
+            navbar(),
+            rx.center(
+                rx.vstack(
+                    header(),
+                    links(),
+                    max_width=styles.MAX_WIDTH,
+                    width="100%",
+                    margin_y=styles.Spacer.BIG.value
+                ),
+            ),
+
+            footer()
     )
+
 
 
 app = rx.App()
